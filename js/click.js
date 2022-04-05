1277017869c54c4abf2bf5e691b6b426d043bb2d
TheFrederick <thefrederick.dev@gmail.com>
2022-04-05 22:57:20 +0200

    Update click.js

--- js/click.js
+++ js/click.js
@@ -4,15 +4,13 @@ function getClickedCircles(e, clicked) {
     // Returns all points clicked by user
 
     return points.filter(p => {
-        const xs = p.x;
-        const ys = p.y;
-        const xb = e.clientX;
-        const yb = e.clientY;
+        const x = e.clientX - p.x;
+        const y = e.clientY - p.y;
 
         // Length of a vector :)
         if (clicked) {
-            return Math.sqrt(xb ** 2 - 2 * xb * xs + xs ** 2 + yb ** 2 - 2 * yb * ys + ys ** 2) <= canvas.height / 50;
+            return Math.sqrt(x**2 + y**2) <= canvas.height / 50;
         }
-        return Math.sqrt(xb ** 2 - 2 * xb * xs + xs ** 2 + yb ** 2 - 2 * yb * ys + ys ** 2) >= canvas.height / 50;
+        return Math.sqrt(x**2 + y**2) > canvas.height / 50;
     })
 }
